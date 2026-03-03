811abc862854ff423855b75e1ce8d112891b038e
Caio Novaes <caionovaes2207@gmail.com>
2026-03-02 23:34:16 -0300

    Update lista_de_exercicio2.java

--- DS/lista_de_exercicio2.java
+++ DS/lista_de_exercicio2.java
@@ -6,20 +6,24 @@ public class lista_de_exercicio2 {
 
         double valor1, valor2, resultado;
 
+        // le os dois valores digitados
         valor1 = Double.parseDouble(
                 JOptionPane.showInputDialog("Digite o primeiro valor:"));
 
         valor2 = Double.parseDouble(
                 JOptionPane.showInputDialog("Digite o segundo valor:"));
 
+        // chama o metodo que calcula a diferença
         resultado = diferenca(valor1, valor2);
 
+        // mostra o resultado
         JOptionPane.showMessageDialog(null,
                 "A diferença é: " + resultado);
     }
 
-    static double diferenca(double v1, double v2) {
+    static double diferenca(double v1, double v2) { // metodo que calcula a diferença do maior para o menor
 
+        // verifica qual numero e maior
         if (v1 > v2) {
             return v1 - v2;
         } else {
@@ -27,4 +31,5 @@ public class lista_de_exercicio2 {
         }
     }
 }
+
 
