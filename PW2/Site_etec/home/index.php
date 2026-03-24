eb1ea6bbae44a1a8e8e241e52244817ae4b2bbfc
Caio Novaes <caionovaes2207@gmail.com>
2026-03-23 21:11:07 -0300

    Refactor HTML structure in index.php

--- PW2/Site_etec/home/index.php
+++ PW2/Site_etec/home/index.php
@@ -36,10 +36,10 @@
 
     <nav class="menu-vermelho">
         <ul>
-            <li><a href="index.php"><?php echo "Home"; ?></a></li>
-            <li><a href="cursos.php"><?php echo "Cursos"; ?></a></li>
-            <li><a href="contato.php"><?php echo "Contato"; ?></a></li>
-            <li><a href="gestao.php"><?php echo "Gestão"; ?></a></li>
+            <li><a href="../home/index.php"><?php echo "Home"; ?></a></li>
+            <li><a href="../cursos/cursos.html"><?php echo "Cursos"; ?></a></li>
+            <li><a href="../contato/contado.php"><?php echo "Contato"; ?></a></li>
+            <li><a href="../gestão/gestao.html"><?php echo "Gestão"; ?></a></li>
         </ul>
     </nav>
 
