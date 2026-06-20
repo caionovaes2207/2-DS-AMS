784e82c08f961fccb55bef80431d98b1d30f2cf2
Caio Novaes <caionovaes2207@gmail.com>
2026-06-19 21:02:05 -0300

    Add files via upload

---
+++ DS/pedra_papel_tesoura/Pedra_papel_tesoura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pedra_papel_tesoura;
+
+/**
+ *
+ * @author Isabelly
+ */
+public class Pedra_papel_tesoura {
+
+    public static void main(String[] args) {
+        
+    }
+}
